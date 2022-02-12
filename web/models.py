74f5f2bce9db67f661c30c139cb57777e7141a87
morteza <mortezaabdollahi1385@gmail.com>
2022-02-12 16:47:56 +0330

    submit/expense and submit/income web services

--- web/models.py
+++ web/models.py
@@ -4,10 +4,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Token(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)    
+
+    def __str__(self):
+        return '{}_token'.format(self.user)
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
-    date = models.DateTimeField()
     amount = models.BigIntegerField()
+    date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -15,8 +22,8 @@ class Expense(models.Model):
         
 class Income(models.Model):
     text = models.CharField(max_length=255)
-    date = models.DateTimeField()
     amount = models.BigIntegerField()
+    date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- web/views.py
+++ web/views.py
@@ -1,3 +1,48 @@
+from urllib.request import Request
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from .models import User, Token, Expense, Income
 # Create your views here.
+
+@csrf_exempt
+def submit_expense(request):
+    """ user submits an expense """
+
+    # TODO: validate data. user might be. token might be fake, amount might be fake...
+    this_token = request.GET.get('token')
+    this_user = User.objects.filter(token__token=this_token).get()
+    date = request.GET.get('date') if 'date' in request.GET else timezone.now()
+    Expense.objects.create(
+        text=request.GET.get('text'),
+        amount=request.GET.get('amount'),
+        date=date,
+        user=this_user
+    )
+
+    return JsonResponse(
+        {'status': 'ok'}, 
+        encoder=DjangoJSONEncoder
+    )
+
+@csrf_exempt
+def submit_income(request):
+    """ user submits an income """
+
+    # TODO: validate data. user might be. token might be fake, amount might be fake...
+    this_token = request.GET.get('token')
+    this_user = User.objects.filter(token__token=this_token).get()
+    date = request.GET.get('date') if 'date' in request.GET else timezone.now()
+    Income.objects.create(
+        text=request.GET.get('text'),
+        amount=request.GET.get('amount'),
+        date=date,
+        user=this_user
+    )
+
+    return JsonResponse(
+        {'status': 'ok'}, 
+        encoder=DjangoJSONEncoder
+    )
